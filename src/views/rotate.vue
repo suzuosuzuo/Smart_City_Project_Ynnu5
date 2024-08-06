908be02a59c625784fb03326bccf586a7ecd7aa3
suzuosuzuo <3493703932@qq.com>
2024-08-07 00:14:43 +0800

    Update rotate.vue

--- src/views/rotate.vue
+++ src/views/rotate.vue
@@ -1,10 +1,32 @@
 <!-- 地球自转 -->
 <template>
-
+    <div>
+    </div>
 </template>
-
 <script setup>
-
+import { inject, onMounted, onBeforeUnmount } from "vue";
+let map, scene;
+onMounted(() => {
+    ({ map, scene } = inject('mapScene'));
+    console.log(map, scene);
+    rotate();
+    map.on("moveend", rotate);
+});
+function rotate() {
+    let center = map.getCenter();
+    center.lng += 1;
+    map.easeTo({
+        center,
+        duration: 500,
+        zoom: 1,
+        pitch:0,
+        bearing: 0,
+        easing: (n) => n,
+    });
+}
+onBeforeUnmount(() => {
+    map.off("moveend", rotate);
+    map.stop();
+});
 </script>
-
 <style scoped></style>
